ebcd7e7bd11060db22e2810a68af891b0ff21648
sabastianrafa <sabastianrafariza@gmail.com>
2025-11-11 16:47:42 +0700

    Memperbaiki navbar dan membuat halaman dashboard

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Sistem Peminjaman Buku')</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100">
+
+    {{-- Navbar --}}
+    @include('partials.navbar')
+
+    {{-- Konten Utama --}}
+    <main class="container mx-auto mt-6">
+        @yield('content')
+    </main>
+
+</body>
+</html>
